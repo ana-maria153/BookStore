b659e5b6de88b2692ce98a44099a73a6cc340931
Iliana8 <iliana.gherta@isa.utm.md>
2024-04-24 12:38:18 +0300

    Home IndexViewModel

---
+++ BookStore/BookStore.Web/Models/HomeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using BookStore.DAL.Models;
+
+namespace BookStore.Web.Models
+{
+	public class HomeIndexViewModel
+	{
+        public List<Book> FeaturedBooks { get; set; }
+        public List<Book> SpecialOfferBooksBooks { get; set; }
+        public List<Book> BillBoardBooks { get; set; }
+        public Book BestSelling { get; set; }
+
+        public HomeIndexViewModel()
+        {
+            FeaturedBooks = new List<Book>();
+            SpecialOfferBooksBooks = new List<Book>();
+            BillBoardBooks = new List<Book>();
+            BestSelling = new Book();
+        }
+    }
+}
