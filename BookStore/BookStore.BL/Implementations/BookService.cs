7d9df07e7de287796a88cbfd183f78b4d88ad9fd
Iliana8 <iliana.gherta@isa.utm.md>
2024-04-24 12:29:52 +0300

    Updated book service

--- BookStore/BookStore.BL/Implementations/BookService.cs
+++ BookStore/BookStore.BL/Implementations/BookService.cs
@@ -1,6 +1,7 @@
 ﻿using BookStore.BL.Interfaces;
 using BookStore.DAL;
 using BookStore.DAL.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.BL.Implementations
 {
@@ -24,8 +25,14 @@ namespace BookStore.BL.Implementations
         }
         public List<Book> GetAllBooks()
         {
-            return _appDBContext.Books.ToList();
+            return _appDBContext.Books.Select(x=>x).Include(x=>x.BookType).ToList();
         }
+
+        public List<Book> GetBooks(int count, int page)
+        {
+            return _appDBContext.Books.Skip(count*(page-1)).Take(count).ToList();
+        }
+
         public Book GetBook(Guid id)
         {
             var book = _appDBContext.Books.Where(x => x.ID == id).FirstOrDefault();
@@ -58,6 +65,102 @@ namespace BookStore.BL.Implementations
                 bookToUpdate.PublicID = resp.PublicId;
             }
             _appDBContext.SaveChanges();
+        }
+
+        public Book GetBook(string pubID)
+        {
+            var book = _appDBContext.Books.Where(x => x.PublicID == pubID).FirstOrDefault();
+            return book!;
+        }
+
+        public void AddBookType(BookType bType)
+        {
+            bType.PublicId = Guid.NewGuid().ToString().Substring(0, 5);
+            _appDBContext.BookTypes.Add(bType);
+            _appDBContext.SaveChanges();
+        }
+
+        public void UpdateBookType(BookType bType)
+        {
+            var bookTypeToUpdate = _appDBContext.BookTypes.FirstOrDefault(x => x.BookTypeID == bType.BookTypeID);
+            if (bookTypeToUpdate == null)
+                throw new Exception("BookType was not found");
+            bookTypeToUpdate.TypeName = bType.TypeName;
+            bookTypeToUpdate.TypeDescription = bType.TypeDescription;
+            _appDBContext.SaveChanges();
+        }
+
+        public void DeleteBookType(Guid id)
+        {
+            var bookTypeToDelete = _appDBContext.BookTypes.FirstOrDefault(x => x.BookTypeID == id);
+            if (bookTypeToDelete == null)
+                throw new Exception("BookType was not found");
+            _appDBContext.BookTypes.Remove(bookTypeToDelete);
+            _appDBContext.SaveChanges();
+        }
+
+        public List<BookType> GetBookTypes()
+        {
+            var types = _appDBContext.BookTypes.ToList();
+            return types;
+        }
+
+        public BookType GetBookType(Guid id)
+        {
+            var bType = _appDBContext.BookTypes.FirstOrDefault(x=>x.BookTypeID == id);
+            return bType;
+        }
+
+        public void LikeBook(string PubId)
+        {
+            var BookToBeLiked = _appDBContext.Books.FirstOrDefault(x => x.PublicID == PubId);
+            if(BookToBeLiked == null)
+                throw new Exception("Book was not found");
+            BookToBeLiked.BookLikes++;
+            _appDBContext.SaveChanges();
+        }
+
+        public BookType GetBookType(string id)
+        {
+            var bType = _appDBContext.BookTypes.FirstOrDefault(x => x.PublicId == id);
+            return bType;
+        }
+
+        public void AddBook(Book ToAdd, string BTID)
+        {
+            var BType = GetBookType(BTID);
+            if (BType == null)
+                throw new Exception("Book Type was not found");
+            ToAdd.BookTypeId = BType.BookTypeID;
+            AddBook(ToAdd);
+        }
+
+        public List<Book> GetBooksByType(string id)
+        {
+            var bookType = GetBookType(id);
+            var books = _appDBContext.Books.Where(x => x.BookTypeId == bookType.BookTypeID).ToList();
+            return books;
+        }
+
+		public List<Book> GetFeaturedBooks()
+		{
+			return _appDBContext.Books.OrderBy(x => Guid.NewGuid()).Take(4).ToList();
+		}
+
+		public List<Book> GetSpecialOfferBooks()
+		{
+			return _appDBContext.Books.OrderBy(x => x.Price).Take(4).ToList();
+		}
+
+        public int GetBookCount()
+        {
+            return _appDBContext.Books.Count();
+        }
+
+        public int BookLikes(string PubId)
+        {
+            var bookLikes = _appDBContext.Books.FirstOrDefault(x => x.PublicID == PubId).BookLikes;
+            return bookLikes;
         }
     }
 }
--- BookStore/BookStore.BL/Interfaces/IBookService.cs
+++ BookStore/BookStore.BL/Interfaces/IBookService.cs
@@ -5,9 +5,28 @@ namespace BookStore.BL.Interfaces
     public interface IBookService
     {
         public void AddBook(Book ToAdd);
+        public void AddBook(Book ToAdd, string BTID);
         public List<Book> GetAllBooks();
+        public List<Book> GetBooks(int count, int page);
         public Book GetBook(Guid id);
+        public Book GetBook(string pubID);
         public void UpdateBook(Book book);
         public void DeleteBook(Guid id);
+
+        //book type
+        public void AddBookType(BookType bType);
+        public void UpdateBookType(BookType bType);
+        public void DeleteBookType(Guid id);
+        public List<BookType> GetBookTypes();
+        public BookType GetBookType(Guid id);
+        public BookType GetBookType(string id);
+        public List<Book> GetBooksByType(string id);
+
+        //others
+        public void LikeBook(string PubId);
+        public int BookLikes(string PubId);
+        public List<Book> GetFeaturedBooks();
+        public List<Book> GetSpecialOfferBooks();
+        public int GetBookCount();
     }
 }
