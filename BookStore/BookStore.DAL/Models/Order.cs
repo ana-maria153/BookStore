b6c26e6cbb875b4bc576c9ed861c971ff38ff987
Iliana8 <iliana.gherta@isa.utm.md>
2024-04-24 12:32:54 +0300

    Order models

---
+++ BookStore/BookStore.DAL/Models/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore.DAL.Models;
+
+public class Order
+{
+    public Guid ID { get; set; }
+    public string Name { get; set; }
+    public string Surename { get; set; }
+    public string Addres { get; set; }
+    public string Phone { get; set; }
+    public List<OrderItem> OrderItems { get; set; }
+}
---
+++ BookStore/BookStore.DAL/Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.DAL.Models;
+
+public class OrderItem
+{
+    [Key]
+    public Guid OrderItemID { get; set; }
+
+    public Guid OrderItemBookID { get; set; }
+    public Book? OrderItemBook { get; set; }
+
+    public int Quantity { get; set; }
+}
