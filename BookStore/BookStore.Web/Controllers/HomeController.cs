80f493df0c184571cc68d0ff648617610babb903
Iliana8 <iliana.gherta@isa.utm.md>
2024-04-24 12:36:41 +0300

    Added home/index ViewModel and like book endpoints

--- BookStore/BookStore.Web/Controllers/HomeController.cs
+++ BookStore/BookStore.Web/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace BookStore.Web.Controllers
     {
         private readonly IBookService _bookService;
 
-        public HomeController(IBookService bookService)
+		public HomeController(IBookService bookService)
         {
             _bookService = bookService;
         }
@@ -17,13 +17,48 @@ namespace BookStore.Web.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            var books = _bookService.GetAllBooks();
-            return View(books);
+            var vm = new HomeIndexViewModel();
+            vm.SpecialOfferBooksBooks = _bookService.GetSpecialOfferBooks();
+            vm.FeaturedBooks= _bookService.GetFeaturedBooks();
+            vm.BillBoardBooks = _bookService.GetBooks(5, 1);
+            vm.BestSelling = _bookService.GetBooks(5, 1).FirstOrDefault();
+			return View(vm);
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public IActionResult BookDetails(string pubId)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                var book = _bookService.GetBook(pubId);
+                if(book != null)
+                    return View(book);
+            }
+            return View("PageNotFound404");
+        }
+
+        [HttpGet]
+        public IActionResult Cart()
+        {
+            return View("Cart");
+        }
+
+        [HttpGet]
+        public IActionResult BookListing(string pubId)
+        {
+            return View("BookListing");
+        }
+
+        [HttpPost]
+        public void LikeBook(string pubId)
+        {
+            _bookService.LikeBook(pubId);
+        }
+        
+        [HttpGet]
+        public int BookLikes(string pubId)
+        {
+            return _bookService.BookLikes(pubId);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
