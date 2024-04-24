d81d91a9c9b504d69f9befb22fe79f364cd22541
Iliana8 <iliana.gherta@isa.utm.md>
2024-04-24 12:34:31 +0300

    added Book Types

---
+++ BookStore/BookStore.DAL/Models/BookType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace BookStore.DAL.Models
+{
+    public class BookType
+    {
+        [Key]
+        public Guid BookTypeID { get; set; }
+        [JsonIgnore]
+        public string? PublicId { get; set; }
+        [DisplayName("Nume tip:")]
+        [Required(ErrorMessage = "Campul este obligatoriu!")]
+        public string TypeName { get; set; }
+        [DisplayName("Descriere tip:")]
+        [Required(ErrorMessage = "Campul este obligatoriu!")]
+        public string TypeDescription { get; set; }
+    }
+}
