308d8d94b01e821d993f4d29e08e309f76539c41
maraqqq <zatictamara@gmail.com>
2024-04-25 12:07:04 +0300

    Updated AdminController and added OrderController

--- BookStore/BookStore.Web/Controllers/AdminController.cs
+++ BookStore/BookStore.Web/Controllers/AdminController.cs
@@ -1,82 +1,156 @@
 ﻿using BookStore.BL.Interfaces;
 using BookStore.DAL.Models;
+using BookStore.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookStore.Web.Controllers
 {
-    [Authorize]
-    public class AdminController : Controller
-    {
-        private readonly IBookService _bookService;
+	[Authorize(Roles = "Admin")]
+	public class AdminController : Controller
+	{
+		private readonly IBookService _bookService;
 
-        public AdminController(IBookService bookService)
-        {
-            _bookService = bookService;
-        }
+		public AdminController(IBookService bookService)
+		{
+			_bookService = bookService;
+		}
 
-        [HttpGet]
-        public IActionResult Index()
-        {
-            var books = _bookService.GetAllBooks();
-            return View(books);
-        }
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var vm = new AdminIndexViewModel();
+			vm.BookCount = _bookService.GetBookCount();
+			return View(vm);
+		} 
 
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View("Add");
-        }
+		[HttpGet]
+		public IActionResult Register()
+		{
+			var vm = new AddBookViewModel();
+			vm.Types = _bookService.GetBookTypes().Select(x => new SelectListItem() { Value = x.PublicId, Text = x.TypeName });
+			if (vm.Types.Count() == 0)
+				return RedirectToAction("RegisterBookType");
+			return PartialView("Add", vm);
+		}
 
-        [HttpGet]
-        public IActionResult Edit(Guid bookId)
-        {
-            if (ModelState.IsValid)
-            {
-                var book = _bookService.GetBook(bookId);
-                return View("Update", book);
-            }
-            return View("Add");
-        }
+		[HttpPost]
+		public IActionResult Register(AddBookViewModel bVm)
+		{
+			if (ModelState.IsValid)
+			{
+				_bookService.AddBook(bVm.Book, bVm.ChosedBookType);
+				return RedirectToAction("BookList");
+			}
+            bVm.Types = _bookService.GetBookTypes().Select(x => new SelectListItem() { Value = x.PublicId, Text = x.TypeName });
+            return View("Add", bVm);
+		}
 
-        [HttpGet]
-        public IActionResult BookList()
-        {
-            var books = _bookService.GetAllBooks();
-            return View(books);
-        }
 
-        [HttpPost]
-        public IActionResult Update(Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _bookService.UpdateBook(book);
-                return RedirectToAction("BookList");
-            }
-            return View("Update", book);
-        }
+		[HttpGet]
+		public IActionResult RegisterBookType()
+		{
+			return View("AddBookType");
+		}
+		//Edit-----------------------------------------------
+		[HttpGet]
+		public IActionResult Edit(string bookId)
+		{
+			if (ModelState.IsValid)
+			{
+				var book = _bookService.GetBook(bookId);
+				if (book != null)
+					return PartialView("Update", book);
+			}
+			return View("PageNotFound404");
+		}
 
-        [HttpPost]
-        public IActionResult Register(Book book)
-        {
-            if (ModelState.IsValid)
-            {
-                _bookService.AddBook(book);
-                return RedirectToAction("BookList");
-            }
-            return View("Add", book);
-        }
+		[HttpPost]
+		public IActionResult Update(Book book)
+		{
+			if (ModelState.IsValid)
+			{
+				_bookService.UpdateBook(book);
+				return RedirectToAction("BookList");
+			}
+			return PartialView("Update", book);
+		}
+		//Edit-----------------------------------------------
 
-        [HttpPost]
-        public IActionResult Delete(Guid id)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-            _bookService.DeleteBook(id);
-            return RedirectToAction("BookList");
-        }
-    }
+		[HttpGet]
+		public IActionResult EditBookType(string bookTypeId)
+		{
+			if (ModelState.IsValid)
+			{
+				var bType = _bookService.GetBookType(bookTypeId);
+				if (bType != null)
+					return View("UpdateBookType", bType);
+			}
+			return View("PageNotFound404");
+		}
+
+
+		[HttpGet]
+		public IActionResult BookList()
+		{
+			var books = _bookService.GetAllBooks();
+			return View(books);
+		}
+
+		[HttpGet]
+		[Authorize(Roles = "Admin, Manager")]
+		public IActionResult BookTypesList()
+		{
+			var types = _bookService.GetBookTypes();
+			return View(types);
+		}
+
+		[HttpPost]
+		public IActionResult UpdateBookType(BookType BType)
+		{
+			if (ModelState.IsValid)
+			{
+				_bookService.UpdateBookType(BType);
+				return RedirectToAction("BookTypesList");
+			}
+			return View("EditBookType", BType.PublicId);
+		}
+
+
+		[HttpPost]
+		public IActionResult RegisterBookType(BookType bType)
+		{
+			if (ModelState.IsValid)
+			{
+				_bookService.AddBookType(bType);
+				return RedirectToAction("BookTypesList");
+			}
+			return View("AddBookType", bType);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(string id)
+		{
+			if (ModelState.IsValid)
+			{
+				var bType = _bookService.GetBook(id);
+				if (bType != null)
+					_bookService.DeleteBook(bType.ID);
+				return RedirectToAction("BookList");
+			}
+			return View();
+		}
+		[HttpPost]
+		public IActionResult DeleteBookType(string id)
+		{
+			if (ModelState.IsValid)
+			{
+				var ToDelete = _bookService.GetBookType(id);
+				_bookService.DeleteBookType(ToDelete.BookTypeID);
+				return RedirectToAction("BookTypesList");
+			}
+			return View();
+		}
+	}
 }
---
+++ BookStore/BookStore.Web/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using BookStore.BL.Interfaces;
+using BookStore.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookStore.Web.Controllers;
+
+public class OrderController : ControllerBase
+{
+    private readonly IOrderService _orderService;
+    private readonly IBookService _bookService;
+
+    public OrderController(IOrderService orderService, IBookService bookService)
+    {
+        _orderService = orderService;
+        _bookService = bookService;
+    }
+
+    [HttpPost]
+    public IActionResult ReceiveOrder([FromBody]OrderRequestViewModel order)
+    {
+        _orderService.PlaceOrder(order.OrderID, order.Name, order.Surname, order.Address, order.Phone, order.OrderProducts.Select(x => new Tuple<string, int>(x.ProdId, x.Quantity)).ToList());
+        return Ok();
+    }
+    [HttpGet]
+    public OrderRequestViewModel ReceiveOrder()
+    {
+        return new OrderRequestViewModel()
+        {
+            Name = "Dora",
+            Surname = "Fax",
+            Phone = "068841556",
+            Address = "Chisinau, Rascani, Studentilor 5",
+            OrderID = Guid.NewGuid(),
+            OrderProducts = new List<OrderProductModel>()
+            {
+                new OrderProductModel()
+                {
+                    ProdId = "SomeProdID1",
+                    Quantity = 10
+                },
+                new OrderProductModel()
+                {
+                    ProdId = "SomeProdID2",
+                    Quantity = 3
+                }
+            }
+        };
+    }
+
+
+    [HttpGet]
+    public CartBookDetailsViewModel? CartBookDetail(string pubID)
+    {
+        var book = _bookService.GetBook(pubID);
+        if (book == null)
+            return null;
+        return new CartBookDetailsViewModel { ImgSrc = book.Path, Author = book.Author, BookName = book.Title, PubId = pubID, Price = book.Price };
+    }
+}
